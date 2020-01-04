4c240b9a107f745e8df12aeb7bd9663ac50d3bf6
farzaneh <farzaneh.dadashi@yahoo.com>
2020-01-04 20:26:36 +0330

    add dakheli page

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$(window).on("resize", function () {
-
-    var b = $(".bg-offerGray").outerHeight(true);
-    var c = $(".bg-offRed").outerHeight(true);
-
-    $("#advertising").css("height", b + c -25);
-}).trigger("resize");
--- js/webslidemenu.js
+++ js/webslidemenu.js
@@ -102,3 +102,11 @@ $( document ).ready(function() {
 
     //------
 });
+
+$(window).on("resize", function () {
+
+    var b = $(".bg-offerGray").outerHeight(true);
+    var c = $(".bg-offRed").outerHeight(true);
+
+    $("#advertising").css("height", b + c -25);
+}).trigger("resize");
